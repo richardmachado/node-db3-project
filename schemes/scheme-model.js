5941d55c4a42b5bd6327b4158bcae018397aebad
Lambda_School_Loaner_198 <lambda_school_loaner_198@Lambda-School-Loaner-198s-MacBook-Pro.local>
2020-02-20 08:06:54 -0800

    couldn't get put or post for steps to work

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,49 +1,57 @@
-const db = require ("../data/db-config.js")
+const db = require('../data/db-config.js')
 
 module.exports = {
-    find,
-    findById,
-    findSteps,
-    add,
-    update,
-    remove
+     find,
+     findById,
+     findSteps,
+     add,
+     update,
+     remove
 }
 
+//* GET all schemes */
 function find(){
-    return db('schemes');
+     return db('schemes');
 }
 
-function findById(){
-return db('schemes')
-.where({id})
-.first();
+//* GET scheme by ID */
+function findById(id){
+     return db('schemes')
+     .where({id})
+     .first();
 }
 
+//* GET steps by scheme ID */
 function findSteps(id){
-    return db.select('steps.id', 'schemes.schemes_name','steps.step_number', 'steps.instructions')
-    .from ('schemes')
-    .join('steps', 'schemes.id', '=', 'steps.scheme_id')
+     return db.select('steps.id', 'schemes.scheme_name','steps.step_number', 'steps.instructions')
+     .from('schemes')
+     .join('steps', 'schemes.id', '=', 'steps.scheme_id')
 }
+
+//* POST new scheme */
 
 function add(scheme){
-    return db('schemes')
-    .insert(scheme)
-    .then(ids => {
-        console.log(ids)
-        console.log(ids[0])
-        return findById(ids[0])
-    })
+     return db('schemes')
+     .insert(scheme)
+     .then(ids => {
+          console.log(ids)
+          console.log(ids[0])
+          return findById(ids[0])
+     })
 }
 
+
+// returns an id 
 function update(changes, id){
-    return db('schemes')
-    .where({id})
-    .update(changes)
-    .then(response => findById(id))
+     return db('schemes')
+     .where({id})
+     .update(changes)
+     .then(response => findById(id))
 }
+
 
 function remove(id){
-    return db('schemes')
-    .where({id})
-    .del()
+     return db('schemes')
+     .where({id})
+     .del()
 }
